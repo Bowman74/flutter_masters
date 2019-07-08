0d153c28125fdba0713797964f00ab0bf41fdb4b
Kevin Ford <4380198+Bowman74@users.noreply.github.com>
2019-07-08 12:57:28 -0700

    Added async/await and REST api call

--- lib/futurepage.dart
+++ lib/futurepage.dart
@@ -1,5 +1,8 @@
 
+import 'dart:io';
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 class FuturePage extends StatefulWidget {
   FuturePage({Key key, this.title}) : super(key: key);
@@ -11,6 +14,7 @@ class FuturePage extends StatefulWidget {
 }
 
 class FuturePageState extends State<FuturePage> {
+  String apiJson = '';
 
   @override
   Widget build(BuildContext context) {
@@ -24,22 +28,57 @@ class FuturePageState extends State<FuturePage> {
             children: <Widget>[
               Expanded(
                 child: Center(
+                  child: Text(apiJson)
                 ),
               ),
               FlatButton(
                 child: 
-                  Text('Run Future'),
-                onPressed: () {
+                  Text('async future'),
+                onPressed: () async {
                   int a = 1;
-                  Future(() {
+                  await Future(() {
                     debugPrint(a.toString());
                   });
                   a++;
                 },
-              )
+              ),
+              FlatButton(
+                child: 
+                  Text('then future'),
+                onPressed: () {
+                  int a = 1;
+                  Future(() {
+                    debugPrint(a.toString());
+                  }).then((_) {
+                    a++;
+                  });
+                },
+              ),
+              FlatButton(
+                child: 
+                  Text('async API call'),
+                onPressed: () async {
+                  var result = await getCatInfo();
+                  setState(() {
+                    apiJson = result;
+                  });
+                },
+              ),
             ]
           ),
         )
       );
+  }
+
+  Future<String> getCatInfo() async {
+    String returnValue = '';
+    var httpClient = new HttpClient();
+    var uri = new Uri.https('cat-fact.herokuapp.com', '/facts');
+    var request = await httpClient.getUrl(uri);
+    var response = await request.close();
+    if (response.statusCode == 200) {
+      returnValue = await response.transform(utf8.decoder).join();
+    }
+    return returnValue;
   }
 }
